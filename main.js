35ea56b0eb4657a216c845b6a435c8d078af256a
mahmoudT95 <mahmoud.t1995@gmail.com>
2022-09-21 17:25:27 +0200

    .

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 var burger = document.querySelector('.burger')
+var side = document.querySelector('.sidecont')
 burger.addEventListener('click',function(){
     burger.classList.toggle('open')
+    side.classList.toggle('sidecontopen')
+    
 })
